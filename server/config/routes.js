6d1142353b27e1d950f28fee5516c3a71ca447bf
unknown <mumairafzal2010@gmail.com>
2020-02-10 19:36:57 -0500

    revert changes to routes.js

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,24 +3,8 @@ const areaHandler = require('../areas/areaHandler.js');
 
 //Add/export api routes
 module.exports = function(app, express){
-    app.get('/', function(req, res){
-        res.redirect('/api/getCrowd');
-     }, dayHandler.getCrowd);
-    
-     app.get('/', function(req, res){
-        res.redirect('/api/getAreaCrowds');
-     }, areaHandler.getAreaCrowds);
-     
-     app.put('/', function(req, res){
-        res.redirect('/api/updateAreaCrowd');
-     }, areaHandler.updateAreaCrowd);
-
-     app.put('/', function(req, res){
-        res.redirect('/api/checkOut');
-     }, areaHandler.checkOut);
-    
-    // app.get('/api/getCrowd', dayHandler.getCrowd);
-    // app.get('/api/getAreaCrowds', areaHandler.getAreaCrowds);
-    // app.put('/api/updateAreaCrowd', areaHandler.updateAreaCrowd);
-    // app.put('/api/checkOut', areaHandler.checkOut);
+    app.get('/api/getCrowd', dayHandler.getCrowd);
+    app.get('/api/getAreaCrowds', areaHandler.getAreaCrowds);
+    app.put('/api/updateAreaCrowd', areaHandler.updateAreaCrowd);
+    app.put('/api/checkOut', areaHandler.checkOut);
 };
